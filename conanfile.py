c282eace8b87df7f4724937e2ab2e90f948ad106
Timur Baidusenov <baidiusenov.tb@phystech.edu>
2025-04-02 00:26:39 +0300

    refactor ci

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 import os
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-from conan.tools.files import replace_in_file
+
 
 class graphRecipe(ConanFile):
     name = "bitonic_sort"
@@ -18,7 +18,6 @@ class graphRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    tool_requires = "cmake/[>=3.29]"
     requires = "opencl-icd-loader/2023.12.14"
     test_requires = "gtest/1.15.0"
 
@@ -42,24 +41,11 @@ class graphRecipe(ConanFile):
         build_type = str(self.settings.build_type)
         self.folders.build = f"build/{build_type}"
         self.folders.generators = f"build/{build_type}/generators"
-    
-    def source(self):
-        if "opencl-icd-loader" in self.requires:
-            cl_loader_dir = os.path.join(self.source_folder, "OpenCL-ICD-Loader")
-            self.run(f"git clone --depth 1 https://github.com/KhronosGroup/OpenCL-ICD-Loader.git {cl_loader_dir}")
-            cmake_file = os.path.join(cl_loader_dir, "CMakeLists.txt")
-            replace_in_file(
-                self, 
-                cmake_file,
-                "cmake_minimum_required(VERSION 3.5)",
-                "cmake_minimum_required(VERSION 3.15)"
-            )
 
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
-        tc.variables["CMAKE_POLICY_DEFAULT_CMP0091"] = "NEW"
         tc.generate()
 
         # Delete CMakeUserPresets.json
